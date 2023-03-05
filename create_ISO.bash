5fc5f8e040a4badb84ab9618183fdcf64323c433
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-03-05 16:34:24 +0100

    edit efiboot.img

--- create_ISO.bash
+++ create_ISO.bash
@@ -3,7 +3,7 @@
 mkdir temp
 mount xcp-ng* temp
 cp -a temp/. iso
-chmod -R 777 iso
+chmod -R 644 iso
 umount temp
 
 cd iso
@@ -12,17 +12,27 @@ cd install
 bunzip2 < ../install.img | cpio -idm
 cd ..
 
-sed -i 's+/boot/vmlinuz console=hvc0 console=tty0+/boot/vmlinuz console=hvc0 console=tty0 answerfile=file:///answerfile.xml install+g' \
-./boot/isolinux/isolinux.cfg \
-./EFI/xenserver/grub.cfg \
-./EFI/xenserver/grub-usb.cfg
-
 cp ../answerfile.xml ./install/
 
 cd install
 find . | cpio -o -H newc | bzip2 > ../install.img
 cd ..
 rm -rf install
+
+cd boot
+mkdir temp
+mcopy -si efiboot.img :: ./temp
+cd ..
+
+sed -i 's+/boot/vmlinuz console=hvc0 console=tty0+/boot/vmlinuz console=hvc0 console=tty0 answerfile=file:///answerfile.xml install+g' \
+./boot/isolinux/isolinux.cfg \
+./EFI/xenserver/grub.cfg \
+./EFI/xenserver/grub-usb.cfg
+./boot/temp/efi/xenserver/grub.cfg
+
+mdeltree -i efiboot.img ::
+mcopy -i efiboot.img -s ./temp/efi ::
+rm -rf temp
 
 OUTPUT=../xcp-ng_custom.iso
 VERSION=8.3
